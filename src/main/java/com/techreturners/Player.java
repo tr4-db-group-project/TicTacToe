50321a7e01eaec2a88053f607809459886352266
Smitha <smitha.kotian@gmail.com>
2024-07-06 23:40:39 +0100

    refactor: refactored code to store player position under player class to have reusable code

---
+++ src/main/java/com/techreturners/Player.java
@@ -0,0 +1,19 @@
+package com.techreturners;
+
+import java.util.List;
+
+public class Player {
+    private List<Position> playerPositions;
+
+    public List<Position> getPlayerPositions() {
+        return playerPositions;
+    }
+
+    public void setPlayerPositions(List<Position> playerPositions) {
+        this.playerPositions = playerPositions;
+    }
+
+    public Player(List<Position> playerPositions) {
+        this.playerPositions = playerPositions;
+    }
+}
--- src/main/java/com/techreturners/TicTacToe.java
+++ src/main/java/com/techreturners/TicTacToe.java
@@ -1,67 +1,63 @@
 package com.techreturners;
 
-import java.util.Arrays;
-import java.util.stream.Stream;
-
 public class TicTacToe {
 
-    private int[][] player1Positions;
-    private int[][] player2Positions;
+    private Player playerOne;
+    private Player playerTwo;
 
-    public int[][] getPlayer1Positions() {
-        return player1Positions;
+    public TicTacToe(Player playerOne, Player playerTwo) {
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
     }
 
-    public void setPlayer1Positions(int[][] player1Positions) {
-        this.player1Positions = player1Positions;
+    public Player getPlayerOne() {
+        return playerOne;
     }
 
-    public int[][] getPlayer2Positions() {
-        return player2Positions;
+    public void setPlayerOne(Player playerOne) {
+        this.playerOne = playerOne;
     }
 
-    public void setPlayer2Positions(int[][] player2Positions) {
-        this.player2Positions = player2Positions;
+    public Player getPlayerTwo() {
+        return playerTwo;
     }
 
-    TicTacToe() {
-        player1Positions = new int[9][];
-        player1Positions = new int[9][];
+    public void setPlayerTwo(Player playerTwo) {
+        this.playerTwo = playerTwo;
+    }
+//
+//    public int[] getPlayer1Position(int row, int column) {
+//        int[] player1 = new int[]{row, column};
+//        return player1;
+//    }
+
+    public boolean validatePositionInput(int rowNum, int colNum) {
+        return (rowNum >= 0 && rowNum < 3 && colNum >= 0 && colNum < 3);
     }
 
-
-    public int[] getPlayer1Position(int row, int column) {
-        int[] player1 = new int[]{row, column};
-        return player1;
-    }
-
-    public boolean validatePositionInput(int[] playerPosition) {
-        if (playerPosition[0] < 3 && playerPosition[1] < 3 && playerPosition[0] >= 0 && playerPosition[1] >= 0)
-            return true;
-        return false;
-    }
-
-    public boolean checkPositionPlayable(int[] playerPosition) {
+    public boolean checkPositionPlayable(int rowNum, int colNum) {
 //        if ((Arrays.stream(getPlayer1Positions()).toList().contains(playerPosition)) ||
 //                (Arrays.stream(getPlayer2Positions()).toList().contains(playerPosition)))
 //            return false;
 //        else
 //            return true;
-        for(int i=0; i<player1Positions.length;i++) {
-            if (Arrays.equals(Arrays.stream(player1Positions[i]).toArray(), playerPosition))
+        for (Position pos : playerOne.getPlayerPositions()) {
+            if ((pos.rowNum() == rowNum) && (pos.colNum() == colNum)) {
                 return false;
+            }
         }
-        for(int i=0; i<player2Positions.length;i++) {
-            if (Arrays.equals(Arrays.stream(player2Positions[i]).toArray(), playerPosition))
+
+        for (Position pos : playerTwo.getPlayerPositions()) {
+            if ((pos.rowNum() == rowNum) && (pos.colNum() == colNum)) {
                 return false;
+            }
         }
-        return  true;
-
+        return true;
     }
-
-    public void savePlayer1Position(int[] playerPosition) {
-        player1Positions[0] = playerPosition;
-
-    }
+//
+//    public void savePlayer1Position(int[] playerPosition) {
+//        player1Positions[player1Positions.length] = playerPosition;
+//
+//    }
 
 }
